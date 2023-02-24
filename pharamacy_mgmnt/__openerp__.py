a696e750276614e02121dca03c0f4e1fcccf3378
hiworth <hiworth.hiworth@gmail.com>
2023-02-24 10:18:26 +0530

    purchase

---
+++ pharamacy_mgmnt/__openerp__.py
@@ -0,0 +1,31 @@
+
+{
+    'name': "Pharmacy Management",
+
+    'summary': """Pharmacy Management
+        """,
+
+
+    'description': """
+        Pharmacy Management.
+    """,
+
+    'author': "Hiworth Solutions",
+    'website': "http://www.hiworthsolutions.com",
+    'license': "AGPL-3",
+
+    'category': 'Sales',
+    'version': '8.0.1.0.0',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base','sale','report_xlsx'],
+
+    # always loaded
+    'data': [
+
+    ],
+    'images': [
+
+    ],
+    'installable': True,
+}
